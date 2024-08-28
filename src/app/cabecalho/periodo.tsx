483a42d95cb0225f6688255e7531a3d45495876b
Gabriel Bello <gassis574@gmail.com>
2024-08-28 11:17:08 -0300

    Update - Alinhamento do componente

--- src/app/cabecalho/periodo.tsx
+++ src/app/cabecalho/periodo.tsx
@@ -1,15 +1,33 @@
-'use client'
+"use client";
 
-import { useGlobalContext } from "../contextGlobal"
+import { useGlobalContext } from "../contextGlobal";
 import { VerificaPeriodo } from "@/actions/orcamentoActions";
-import { retPeriodoAtual } from '@/lib/formatacoes';
+import { retPeriodoAtual } from "@/lib/formatacoes";
 import { Label } from "@/components/ui/label";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { useEffect } from "react";
-//import queryClient from "@/lib/reactQuery";
-//import {useSession } from "next-auth/react"
 
-const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro",];
+const meses = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
 
 function obterMesAno() {
   const anoAtual = new Date().getFullYear();
@@ -20,61 +38,60 @@ function obterMesAno() {
     }
   }
   return mesAno;
-};
+}
 
 export default function Periodo() {
-  
+  const { usuarioId, periodo, setPeriodo, periodoId, setPeriodoId } =
+    useGlobalContext();
 
-  //const { data: session } = useSession();
-
-  const { usuarioId, periodo, setPeriodo, periodoId, setPeriodoId} = useGlobalContext();
-
-  useEffect(()=>{
-      const fetchData = async () =>{
-        if(periodoId < 1){
-          const atualPeriodo = retPeriodoAtual();
-          const resultado = await VerificaPeriodo(usuarioId, atualPeriodo);
-          setPeriodo(retPeriodoAtual())
-          setPeriodoId(resultado.regId)
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      if (periodoId < 1) {
+        const atualPeriodo = retPeriodoAtual();
+        const resultado = await VerificaPeriodo(usuarioId, atualPeriodo);
+        setPeriodo(retPeriodoAtual());
+        setPeriodoId(resultado.regId);
       }
-      fetchData();
-  })
-  
-  const onChange = async (value: string) =>{
+    };
+    fetchData();
+  }, [periodoId, usuarioId, setPeriodo, setPeriodoId]);
+
+  const onChange = async (value: string) => {
     const resultado = await VerificaPeriodo(usuarioId, value);
-    if(resultado.status === "Sucesso"){
+    if (resultado.status === "Sucesso") {
       setPeriodoId(resultado.regId);
       setPeriodo(value);
     }
-    return true
-  }
+    return true;
+  };
 
-
-
-  return(
-    <div className="flex flex-row justify-center align-middle">
-      <div className="flex mr-2 align-middle">
-        <Label className="block h-[40px] font-bold text-xl text-sky-50">
-            Período Ativo
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <div className="mr-2">
+        <Label className="h-[40px] font-bold text-xl text-sky-50 flex items-center">
+          Período Ativo
         </Label>
       </div>
-      <div className="flex ">
+      <div className="w-[250px]">
         <Select defaultValue={periodo} onValueChange={onChange}>
-          <SelectTrigger className="w-[250px] h-[40px] text-xl">
+          <SelectTrigger className="w-full h-[40px] text-xl flex items-center">
             <SelectValue placeholder="Selecione o periodo" />
           </SelectTrigger>
-          <SelectContent className="h-[200px] opacity-100 border-2 border-sky-950 bg-sky-100 text-sky-900">
+          <SelectContent className="max-h-[200px] opacity-100 border-2 border-sky-950 bg-sky-100 text-sky-900">
             <SelectGroup className="hover:bg-sky-900 hover:text-sky-100">
-              { obterMesAno().map((mesAno, index) => (
-                <SelectItem className="bg-sky-100  text-sky-900"  key={index} value={mesAno}>{mesAno}</SelectItem>
-                ))
-              }                  
+              {obterMesAno().map((mesAno, index) => (
+                <SelectItem
+                  className="bg-sky-100 text-sky-900"
+                  key={index}
+                  value={mesAno}
+                >
+                  {mesAno}
+                </SelectItem>
+              ))}
             </SelectGroup>
           </SelectContent>
         </Select>
       </div>
-    </div> 
-  )
-
+    </div>
+  );
 }
