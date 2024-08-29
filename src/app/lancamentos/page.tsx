83dcddacd6bcdc68bfbdd05aeeb8bbe1ed0cd014
Gabriel Bello <gassis574@gmail.com>
2024-08-29 10:04:15 -0300

    Update - Página lancamento, min-h-screen

--- src/app/lancamentos/page.tsx
+++ src/app/lancamentos/page.tsx
@@ -6,13 +6,13 @@ import PainelFiltros from "./_components/painelFiltros";
 export default async function LancamentosPage() {
   return (
     <LancamentoProvider>
-      <div className="overflow-x-auto min-w-screen  w-[90%] max-w-[1400px] min-w-[500px]">
-        <div className="justify-items-center flex flex-col min-h-[80vh]md:justify-center md:px-6 md:gap-5">
+      <div className="overflow-x-auto min-w-screen w-[90%] min-h-screen max-w-[1400px] min-w-[500px]">
+        <div className="justify-items-center flex flex-col md:justify-center md:px-6 md:gap-5">
           <div className="flex flex-col w-full gap-4">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center text-sky-900">
               Grupos de Lançamentos
             </h1>
-            <p className="text-sky-800 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-center ">
+            <p className="text-sky-800 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed text-center mb-6">
               Gerenciar os lançamentos para o controle financeiro
             </p>
           </div>
